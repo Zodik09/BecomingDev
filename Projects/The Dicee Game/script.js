2b89faaa0dc3c5ad67397a1b50725b1ecf862a80
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2024-12-07 20:15:03 +0530

    Completed the project by adding the dynamic announcement if any player wins or if the match draws.

--- Projects/The Dicee Game/script.js
+++ Projects/The Dicee Game/script.js
@@ -6,3 +6,20 @@ var img1Src2 = "./images/dice" + randomNumber2 + ".png";
 
 var img1 = document.querySelector(".dice .img1").setAttribute("src", img1Src1);
 var img2 = document.querySelector(".dice .img2").setAttribute("src", img1Src2);
+
+var h1 = document.querySelector(".container h1");
+console.log(h1)
+
+if(randomNumber1 === randomNumber2){
+    h1.textContent = "Draw";
+}
+else if(randomNumber1 === 6){
+    h1.textContent = "Player 1 wins!";
+}
+else if(randomNumber2 === 6){
+    h1.textContent = "Player 2 wins!";
+}
+
+else{
+    h1.textContent = "Refresh Me";
+}
